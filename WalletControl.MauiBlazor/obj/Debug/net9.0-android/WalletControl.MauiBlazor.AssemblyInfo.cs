5c7d2c5e7cb7b131ce0f70ac431937f84aaf0d68
EduardoGubert <edugubertnascimento@gmail.com>
2025-02-25 18:04:49 -0400

    Remove binary files from version control and update .gitignore to exclude build artifacts

--- WalletControl.MauiBlazor/obj/Debug/net9.0-android/WalletControl.MauiBlazor.AssemblyInfo.cs
+++ WalletControl.MauiBlazor/obj/Debug/net9.0-android/WalletControl.MauiBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WalletControl.MauiBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4ffb7f0c45f8b470dee63d2cc3324e00a36be54c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WalletControl.MauiBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WalletControl.MauiBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
